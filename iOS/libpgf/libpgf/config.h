c3b8bf8172240019ee56e2f10cd46f6851982e62
Emil Djupfeldt <emil@djupfeldt.se>
2012-06-14 13:29:31 +0200

    libpgf now compiles in Xcode.

---
+++ iOS/libpgf/libpgf/config.h
@@ -0,0 +1,14 @@
+//
+//  config.h
+//  libpgf
+//
+//  Created by Emil Djupfeldt on 2012-06-14.
+//  Copyright (c) 2012 Chalmers. All rights reserved.
+//
+
+#ifndef libpgf_config_h
+#define libpgf_config_h
+
+
+
+#endif
