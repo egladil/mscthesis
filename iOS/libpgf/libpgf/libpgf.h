51d2e847da93fbb8cb5cb15c13efb1d0259b6474
Emil Djupfeldt <emil@djupfeldt.se>
2012-06-14 12:48:23 +0200

    Created XCode projeject for libpgf.

---
+++ iOS/libpgf/libpgf/libpgf.h
@@ -0,0 +1,13 @@
+//
+//  libpgf.h
+//  libpgf
+//
+//  Created by Emil Djupfeldt on 2012-06-14.
+//  Copyright (c) 2012 Chalmers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libpgf : NSObject
+
+@end
---
+++ iOS/libpgf/libpgf/libpgf.m
@@ -0,0 +1,23 @@
+//
+//  libpgf.m
+//  libpgf
+//
+//  Created by Emil Djupfeldt on 2012-06-14.
+//  Copyright (c) 2012 Chalmers. All rights reserved.
+//
+
+#import "libpgf.h"
+
+@implementation libpgf
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
