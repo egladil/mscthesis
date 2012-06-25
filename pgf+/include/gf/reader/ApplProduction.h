4473f394d1ec386e7a1cc0c0c0fdea1e73554ead
Emil Djupfeldt <emil@djupfeldt.se>
2012-06-25 15:24:58 +0200

    Added c++ interfaces for productions.

---
+++ pgf+/include/gf/reader/ApplProduction.h
@@ -0,0 +1,42 @@
+//
+//  ApplProduction.h
+//  pgf+
+//
+//  Created by Emil Djupfeldt on 2012-06-25.
+//  Copyright (c) 2012 Chalmers University of Technology. All rights reserved.
+//
+
+#ifndef pgf__ApplProduction_h
+#define pgf__ApplProduction_h
+
+#include <stdint.h>
+#include <vector>
+#include <gf/reader/CncFun.h>
+#include <gf/reader/Production.h>
+
+namespace gf {
+    namespace reader {
+        
+        class ApplProduction : public Production {
+        private:
+            /* final */ CncFun* function;
+            /* final */ std::vector<uint32_t> domain;
+            
+        public:
+            ApplProduction(uint32_t fId, CncFun* function, const std::vector<uint32_t>& domain);
+            virtual ~ApplProduction();
+            
+            virtual std::string toString() const;
+            virtual bool equals(const Production* other) const;
+            
+            virtual const std::vector<uint32_t>& getDomain() const;
+            
+            virtual CncFun* getFunction() const;
+            
+            virtual const std::vector<uint32_t>& getArgs() const;
+        };
+        
+    }
+}
+
+#endif
---
+++ pgf+/include/gf/reader/CoerceProduction.h
@@ -0,0 +1,38 @@
+//
+//  CoerceProduction.h
+//  pgf+
+//
+//  Created by Emil Djupfeldt on 2012-06-25.
+//  Copyright (c) 2012 Chalmers University of Technology. All rights reserved.
+//
+
+#ifndef pgf__CoerceProduction_h
+#define pgf__CoerceProduction_h
+
+#include <stdint.h>
+#include <vector>
+#include <gf/reader/Production.h>
+
+namespace gf {
+    namespace reader {
+        
+        class CoerceProduction : public Production {
+        private:
+            uint32_t initId;
+            
+        public:
+            CoerceProduction(uint32_t fId, uint32_t initId);
+            virtual ~CoerceProduction();
+            
+            virtual std::string toString() const;
+            virtual bool equals(const Production* other) const;
+            
+            virtual const std::vector<uint32_t>& getDomain() const;
+            
+            virtual uint32_t getInitId() const;
+        };
+        
+    }
+}
+
+#endif
