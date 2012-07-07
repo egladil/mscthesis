eb771d2605a00e7afcf39c53fb3afa7d240a92cd
Emil Djupfeldt <emil@djupfeldt.se>
2012-07-07 11:44:07 +0200

    Add support for iOS.

---
+++ pgf+/pgf+ios/pgf_ios.h
@@ -0,0 +1,13 @@
+//
+//  pgf_ios.h
+//  pgf+ios
+//
+//  Created by Emil Djupfeldt on 2012-07-07.
+//  Copyright (c) 2012 Chalmers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface pgf_ios : NSObject
+
+@end
---
+++ pgf+/pgf+ios/pgf_ios.m
@@ -0,0 +1,23 @@
+//
+//  pgf_ios.m
+//  pgf+ios
+//
+//  Created by Emil Djupfeldt on 2012-07-07.
+//  Copyright (c) 2012 Chalmers. All rights reserved.
+//
+
+#import "pgf_ios.h"
+
+@implementation pgf_ios
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
